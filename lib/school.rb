063fe570392f160a8be92f8ef199983654856f86
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-03 22:55:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,77 @@
-# code here!
+
+
+
+
+
+
+
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+ def roster
+  @roster
+  end
+
+def add_student(name, grade_level)
+  @roster[grade_level]||= []
+  @roster[grade_level]<< name
+  
+
+end
+
+def grade(grade_level)
+  @roster[grade_level]
+end
+
+def sort
+  new_roster = {}
+  @roster.each do |grade, name|
+    new_roster [grade] = name.sort
+    
+  end
+  new_roster
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+ def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+  end
+
+
+
+end
+end
+=end
+#school = School.new("Bayside High School")
+
+
