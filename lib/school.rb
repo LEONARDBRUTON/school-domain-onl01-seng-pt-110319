c410b28e70c1c0bb3a012132ee2102f81e2df2c8
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-03 22:13:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,60 @@
-# code here!
+
+
+
+
+
+
+
+class School
+  attr_accessor :name, :roster
+  def initilize(name)
+    @name = name
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+
+def add_student(name, grade_level)
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+ def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+  end
+
+
+
+end
+end
+=end
+#school = School.new("Bayside High School")
+
+
